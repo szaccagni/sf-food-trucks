7eca75917dd8d2c159f80b0a216777eabfc5057f
szaccagni <sarahzaccagni@gmail.com>
2023-05-17 16:33:25 -0400

    adds name and description to truck obj

--- controllers/index.js
+++ controllers/index.js
@@ -3,14 +3,34 @@ module.exports = {
     index
 }
 
-const coordinates = [
-    [-122.3917968, 37.77632715],
-    [-122.3996179, 37.79926011],
-    [-122.4583917, 37.76386403],
-    [-122.3884823, 37.76329906],
-    [-122.4002596, 37.77687639]    
+const trucks = [
+    {
+        coordinates: [-122.3917968, 37.77632715],
+        name: 'frankies',
+        sells: 'hot dogs'
+    },
+    {
+        coordinates: [-122.3996179, 37.79926011],
+        name: 'bobs',
+        sells: 'burgers'
+    },
+    {
+        coordinates: [-122.4583917, 37.76386403],
+        name: 'joes',
+        sells: 'pizza'
+    },
+    {
+        coordinates: [-122.3884823, 37.76329906],
+        name: 'vanessas',
+        sells: 'dumplings'
+    },
+    {
+        coordinates: [-122.4002596, 37.77687639],
+        name: 'petes',
+        sells: 'pie'
+    }   
 ]
 
 function index(req, res) {
-    res.render('index', {title: 'Find Food Trucks', mapbox_token:process.env.MAPBOX_TOKEN, search: null, coordinates:coordinates})
+    res.render('index', {title: 'Find Food Trucks', mapbox_token:process.env.MAPBOX_TOKEN, search: null, trucks:trucks})
 }
