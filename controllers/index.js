afc6cc01ebe4ab83607099fd3b63d9f9b85433da
szaccagni <sarahzaccagni@gmail.com>
2023-05-17 12:37:14 -0400

    adds markers to init map

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,14 @@ module.exports = {
     index
 }
 
+coordinates = [
+    [-122.3917968, 37.77632715],
+    [-122.3996179, 37.79926011],
+    [-122.4583917, 37.76386403],
+    [-122.3884823, 37.76329906],
+    [-122.4002596, 37.77687639]    
+]
+
 function index(req, res) {
-    res.render('index', {title: 'Find Food Trucks', mapbox_token:process.env.MAPBOX_TOKEN})
+    res.render('index', {title: 'Find Food Trucks', mapbox_token:process.env.MAPBOX_TOKEN, coordinates:coordinates})
 }
