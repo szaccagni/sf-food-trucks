61cb8bf75e245a97d87dcf001217798fbcb21f98
szaccagni <sarahzaccagni@gmail.com>
2023-05-17 17:10:04 -0400

    adds api call to sf gov data

--- controllers/index.js
+++ controllers/index.js
@@ -1,36 +1,44 @@
+const Truck = require('../models/truck')
 
 module.exports = {
     index
 }
 
+// test data
 const trucks = [
     {
         coordinates: [-122.3917968, 37.77632715],
+        address: '90 BROADWAY', 
         name: 'frankies',
         sells: 'hot dogs'
     },
     {
         coordinates: [-122.3996179, 37.79926011],
+        address: '90 BROADWAY', 
         name: 'bobs',
         sells: 'burgers'
     },
     {
         coordinates: [-122.4583917, 37.76386403],
+        address: '90 BROADWAY', 
         name: 'joes',
         sells: 'pizza'
     },
     {
         coordinates: [-122.3884823, 37.76329906],
+        address: '90 BROADWAY', 
         name: 'vanessas',
         sells: 'dumplings'
     },
     {
         coordinates: [-122.4002596, 37.77687639],
+        address: '90 BROADWAY', 
         name: 'petes',
         sells: 'pie'
     }   
 ]
 
-function index(req, res) {
-    res.render('index', {title: 'Find Food Trucks', mapbox_token:process.env.MAPBOX_TOKEN, search: null, trucks:trucks})
+async function index(req, res) {
+    const allTrucks = await Truck.getAll()
+    res.render('index', {title: 'Find Food Trucks', mapbox_token:process.env.MAPBOX_TOKEN, search: null, trucks:allTrucks})
 }
--- models/truck.js
+++ models/truck.js
@@ -1,5 +1,28 @@
+const token = process.env.SFGOV_DATA_TOKEN
 const BASE_URL = 'https://data.sfgov.org/resource/rqzj-sfat.json'
 
 module.exports =  {
     getAll
+}
+
+async function getAll() {
+    const data = await fetch(BASE_URL, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'X-App-Token': token
+        }
+    })
+    const response = await data.json()
+    cleanedData = []
+    response.forEach(truck => {
+        cleanTruck = {
+            coordinates: [truck.longitude, truck.latitude],
+            address: truck.address, 
+            name: truck.applicant,
+            sells: truck.fooditems
+        }
+        cleanedData.push(cleanTruck)
+    })
+    return cleanedData
 }
