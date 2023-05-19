bad5467b93b9485f3338f60fa5a2adfb02c40b70
szaccagni <sarahzaccagni@gmail.com>
2023-05-19 12:08:33 -0400

    fixing bug

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-const exphbs  = require('express-handlebars');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
