e61d9bcff5cfe138a4bab1a3d8523485fa53abfe
szaccagni <sarahzaccagni@gmail.com>
2023-05-19 12:30:31 -0400

    updates og meta tag image

--- controllers/index.js
+++ controllers/index.js
@@ -6,5 +6,5 @@ module.exports = {
 
 async function index(req, res) {
     const allTrucks = await Truck.getAll()
-    res.render('index', {image: '/images/CURB-CUSINE-SQR.jpeg', description: 'CURB CUISINE SF', title: 'CURB CUISINE SF', mapbox_token:process.env.MAPBOX_TOKEN, search: null, trucks:allTrucks})
+    res.render('index', {image: '/images/CURB-CUISIN-SQR.png', description: 'CURB CUISINE SF', title: 'CURB CUISINE SF', mapbox_token:process.env.MAPBOX_TOKEN, search: null, trucks:allTrucks})
 }
